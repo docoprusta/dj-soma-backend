c76145dd967f970459b0b2a2e96f4159bb055cb8
docoprusta <gyore.soma@gmail.com>
2018-02-21 21:11:02 +0100

    Add socket.io

--- server.py
+++ server.py
@@ -4,62 +4,91 @@ import os
 import signal
 import shlex
 import queue
+
 import threading
 import time
 
+from flask_socketio import emit
+from mpv import MPV
 from flask import Flask
+from flask_socketio import SocketIO
 from flask import request
+from flask import json
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = '0271521ae2e214eb55e6d34d48f1d63754ecf00f7137777a43306fac9da070d5'
+socketio = SocketIO(app, async_mode='threading')
 
 is_first = True
-process = None
 
-
-if os.name == 'posix':
-    mpv_path = 'mpv'
-elif os.name == 'nt':
-    mpv_path = 'windows\\mpv.exe'
-
+playlist = queue.Queue()
 video_ids = queue.Queue()
 
-def play_video():
-    while True:
-        try:
-            os.wait()
-        except:
-            pass
+time_pos = 0
+duration = 0
+prev_time = time.time()
 
-        if not video_ids.empty() and process is not None and not psutil.pid_exists(process.pid):
-            threading.Thread(target=start_process, args=[video_ids.get(timeout=1)]).start()
-        time.sleep(.5)
+player = MPV('no-video', ytdl=True)
+
+
+@player.property_observer('time-pos')
+def print_time_pos(_name, _value):
+    global time_pos
+    global duration
+    global prev_time
+    time_pos = player.osd.time_pos
+    duration = player.osd.duration
+
+    if (time.time() - prev_time) >= 1 and time_pos is not None and duration is not None:
+        time_pos_in_sec = sum(x * int(t) for x, t in zip([3600, 60, 1], time_pos.split(":"))) 
+        duration_in_sec = sum(x * int(t) for x, t in zip([3600, 60, 1], duration.split(":"))) 
+        socketio.emit('message', int((time_pos_in_sec/duration_in_sec)*100), broadcast=True)
+        prev_time = time.time()
+
+    if time_pos == duration and time_pos != 0 and duration != 0:
+        playlist.get()
+        video_id = video_ids.get()
+        player.play('http://www.youtube.com/watch?v={}'.format(video_id))
 
 def start_process(video_id):
-    global process
-    splitted_command = shlex.split('{} --no-video https://www.youtube.com/watch?v={}'.format(mpv_path, video_id))
-    print(splitted_command)
-    process = subprocess.Popen(splitted_command)
-    # process.stdout.read()
+    player.play('http://www.youtube.com/watch?v={}'.format(video_id))
+    send_playtime_in_every_second()
 
-@app.route('/')
-def hello_world():
-    global is_first
-    video_id = request.args.get('video')
-    if is_first:
-        start_process(video_id)
-        threading.Thread(target=play_video).start()
-    else:
-        video_ids.put(video_id, timeout=1)
+
+@app.route('/playlist', methods=['GET'])
+def get_playlist():
+    return json.dumps(list(playlist.queue))
+
+
+def send_playtime_in_every_second():
+    print(time_pos, duration)
+    while True:
+        print(time_pos, duration)
+        if time_pos == duration and time_pos != 0 and duration != 0:
+            break
+        print(is_first)
+        socketio.emit('message', time_pos, broadcast=True)
+        socketio.sleep(1)
+
+
+@app.route('/song', methods=['POST'])
+def post_song():
     
-    # if process is not None and not is_first and psutil.pid_exists(process.pid):
-        # os.kill(process.pid, signal.SIGTERM)
-
-
-    is_first = False
-
+    global is_first
+    posted_dict = request.json
+    playlist.put(posted_dict)
+    video_id = posted_dict.get('youtubeId')
+    video_ids.put(video_id)
+    if is_first:
+        video_id = video_ids.get()
+        player.play('http://www.youtube.com/watch?v={}'.format(video_id))
+        is_first = False
     return 'Hello, World!'
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    try:
+        socketio.run(app, '0.0.0.0')
+    except:
+        pass
